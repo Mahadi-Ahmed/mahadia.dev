2d71b65292736b496f24a5612f5adfc1c705973a
Mahadi-Ahmed <mahadia.dev@gmail.com>
2025-01-26 23:12:19 +0100

    add robots.txt & sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mahadia.dev',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
-    })
+    }),
+    sitemap()
   ],
   build: {
     // Enable minification for all assets
